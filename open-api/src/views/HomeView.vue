3ab6ab4e8560b23fbb47dfe6efec0d87388c9987
AbigailLabanok <abigail.labanok@gmail.com>
2023-04-25 13:10:42 -0400

    hi

---
+++ open-api/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+const stuff = ref('') //with ref, if we need some data to update regularly like with an async function, ref lets you
+async function getStuff() {
+  let result = await fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json')
+  let data = await result.json()
+  console.log(data)
+}
+onMounted(() => {
+  //onMounted lets you run the async function once the componenet is mounted
+  getStuff()
+})
+</script>
+
+<style scoped>
+
+</style>
