5874cfc06f739475cb86043782b67170d3b07eb3
AbigailLabanok <abigail.labanok@gmail.com>
2023-04-26 13:17:18 -0400

    Update HomeView.vue

--- open-api/src/views/HomeView.vue
+++ open-api/src/views/HomeView.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-
+    <DataCards
+      v-for="(dat, index) in data"
+      :key="dat.complaint_type"
+      :id="index + 1"
+      :pokemon="dat"
+    />
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
+import DataCards from '../components/DataCards.vue'
 const stuff = ref('') //with ref, if we need some data to update regularly like with an async function, ref lets you
 async function getStuff() {
   let result = await fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json')
@@ -18,6 +24,4 @@ onMounted(() => {
 })
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
