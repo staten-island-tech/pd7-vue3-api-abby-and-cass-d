985a039951e268cc7f49209aea62e178cf3eefca
AbigailLabanok <abigail.labanok@gmail.com>
2023-05-01 14:27:24 -0400

    Revert "s"
    This reverts commit 409277cc26258d7682024bf53b3d5c5cc254f155.

---
+++ open-api-vids/src/components/PokemonCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <router-link :to="pokemonPath" class="card"></router-link>
+  <h2>{{ pokemon.name }}</h2>
+  <h3>{{ id }}</h3>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+const props = defineProps({
+  pokemon: Object,
+  id: Number
+})
+const pokemonPath = computed(()=>{
+  return `/PokemonData/${props.pokemon.name}` //turns the data into a url
+})
+</script>
+
+<style scoped>
+.card {
+  width: 28%;
+  height: 50rem;
+  background-color: aliceblue;
+  margin: 30px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1rem;
+  text-transform: uppercase;
+}
+img {
+  width: 100%;
+  height: 80%;
+  object-fit: cover;
+}
+</style>
---
+++ open-api-vids/src/views/PokemonData.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>{{ monster.name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      monster: {}
+    }
+  },
+  mounted: async function () {
+    await this.getPokemon()
+  },
+  methods: {
+    getPokemon: async function () {
+      let result = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.$route.params.id}`)
+      let data = await result.json()
+      this.monster = data
+    }
+  }
+}
+</script>
+
+<style scoped></style>
