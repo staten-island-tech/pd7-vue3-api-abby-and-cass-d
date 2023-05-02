28006fd4915612523995c181aed13f135845688c
AbigailLabanok <abigail.labanok@gmail.com>
2023-05-01 22:20:42 -0400

    asd

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -4,6 +4,43 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <RouterView />
+  <nav>
+    <RouterLink to="/">Bar Chart</RouterLink>
+    <RouterLink to="/doughnutchart">Doughnut Chart</RouterLink>
+  </nav>
 </template>
 
-<style scoped></style>
+<style scoped>
+nav {
+  width: 100%;
+  font-size: 5rem;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+nav {
+  margin-left: -1rem;
+  font-size: 5rem;
+
+  padding: 1rem 0;
+  margin-top: 1rem;
+}
+</style>
---
+++ vue/src/views/DoughnutChartView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Number of People Admitted to Hospitals Due to Covid</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+const stuff = ref('') //with ref, if we need some data to update regularly like with an async function, ref lets you
+async function getStuff() {
+  let result = await fetch('https://data.cityofnewyork.us/resource/sj3k-gzyx.json')
+  let data = await result.json()
+  console.log(data)
+}
+onMounted(() => {
+  //onMounted lets you run the async function once the componenet is mounted
+  getStuff()
+})
+</script>
+
+<style scoped></style>
